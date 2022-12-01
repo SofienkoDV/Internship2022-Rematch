148f65a2bd2a13bf97e0749dc78b7ea07725d6eb
SofienkoDV <sofienko2933@gmail.com>
2022-12-01 16:21:14 +0200

    update week1

--- week1.2/express/src/server/index.js
+++
@@ -1,9 +0,0 @@
-/* eslint-disable import/extensions */
-import http from 'http';
-
-import server from './server.js';
-import events from './events.js';
-
-const PORT = server.get('port');
-
-events.bind(http.createServer(server).listen(PORT));
