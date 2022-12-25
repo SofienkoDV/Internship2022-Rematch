d3bbd144d8430e28a5c29193288c6468e2501165
SofienkoDV <sofienko2933@gmail.com>
2022-12-25 23:45:24 +0200

    Small fixed tasks router

--- week1/express/src/components/Tasks/router.js
+++ week1/express/src/components/Tasks/router.js
@@ -7,10 +7,15 @@ const taskComponent = require('./index');
 const router = Router();
 
 router.post('/create', checkAuth, createTaskValidation, handleValidationErrors, taskComponent.createTask);
+
 router.put('/update/:id', checkAuth, taskComponent.updateTask);
+
 router.get('/get', checkAuth, taskComponent.getTasks);
+
 router.get('/get-all', checkAuth, taskComponent.getAllTasks);
+
 router.get('/get/:id', checkAuth, taskComponent.getTask);
+
 router.delete('/delete/:id', checkAuth, taskComponent.deleteTask);
 
 module.exports = router;
