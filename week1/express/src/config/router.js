0da9e3ac9bfd03f07a623b0b92a2accaddb6b856
SofienkoDV <sofienko2933@gmail.com>
2023-01-03 23:26:59 +0200

    Added Swagger UI for API documentation

--- week1/express/src/config/router.js
+++ week1/express/src/config/router.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const http = require('http');
 
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./swagger.json');
+
 const UsersRouter = require('../components/Users/router');
 const TasksRouter = require('../components/Tasks/router');
 
@@ -8,6 +11,7 @@ module.exports = {
     init(app) {
         const router = express.Router();
 
+        app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
         app.use('/v1/users', UsersRouter);
         app.use('/v1/tasks', TasksRouter);
 
